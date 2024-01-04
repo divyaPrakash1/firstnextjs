0710cc455c9dbb95dc7ecd8edfc92b36e6b5e0d0
Divya Prakash Mishra <iamdpm93@gmail.com>
2024-01-04 15:32:53 +0530

    Laoder

---
+++ src/app/users/loading.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>
+                Loading Products List...
+            </h1>
+        </div>
+    )
+}
---
+++ src/app/users/page.js
@@ -0,0 +1,20 @@
+
+async function productList() {
+    let data = await fetch("https://dummyapi.online/api/products");
+    data = await data.json();
+    return data;
+}
+
+export default async function Page() {
+    let products = await productList();
+    return (
+        <div>
+            <h1>Product Name list</h1>
+            {products && products.length > 0 && products.map((product) => (
+                <div>
+                    <h2> Product Name : {product.name} </h2>
+                </div>
+            ))}
+        </div>
+    )
+}
